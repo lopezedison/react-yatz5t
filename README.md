1e4a8340eb6261bd159a7dbfbc2cde17a1a4269e
lopezedison <edisona.lopezv@utadeo.edu.co>
2022-02-23 10:31:54 -0500

    3 files modified, 5 added, 2 deleted

--- README.md
+++
@@ -1,3 +0,0 @@
-# react-t8po7v
-
-[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-t8po7v)
--- src/index.js
+++ src/index.js
@@ -1,6 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
+import {BrowserRouter} from 'react-router-dom';
 import App from "./App";
+import {Provider} from 'react-redux'
+import {createStore} from 'redux'
+import reducer from './reducers/tiendaReducer'
+const store=createStore(reducer);
 
-ReactDOM.render(<App />, document.getElementById("root"));
+
+ReactDOM.render(
+  
+  <BrowserRouter>
+  <Provider store={store}>
+  <App />
+  </Provider>
+  
+  
+  </BrowserRouter>
+  
+  , document.getElementById("root"));
